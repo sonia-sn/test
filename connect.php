d444f92d616c4e9c967339272fbf743c8c533d36
sonia-sn <shetu4530@gtmail.com>
2024-10-31 11:01:01 +0600

    version1

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+//database connection
+$localhost = "localhost";
+
+$user = "root";
+
+$password ="";
+
+$database = "web";
+
+$con = new mysqli($localhost,$user,$password,$database);
+
+
+// Check connection
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+  }
+
+?>
